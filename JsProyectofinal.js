4f3c1edf8a910744bec5aacdc7fb95dc9ecf4145
lautagomez <90482376+lautagomez@users.noreply.github.com>
2023-02-10 21:27:41 -0300

    Add files via upload

---
+++ JsProyectofinal.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function (){
+    console.log("capturamos el evento CLICK");
+    document.getElementById("eventoQuienSoy").innerHTML = "me llamo lautaro tengo 27 años y soy estudiante de la mitad de la carrera de ingenieria en informatica, con 4 años en el area automotriz , 2 en verificacion , y 2 en logistica actualimente";
+}
